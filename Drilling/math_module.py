9098e269cd3548f3fd2b2a915863ec01c89e2fcc
Laura <arnoldlaura24@gmail.com>
2024-12-11 18:15:57 -0800

    Create math_module.py

---
+++ Drilling/math_module.py
@@ -0,0 +1,8 @@
+import math
+
+print('The factorial of 3 is:{}'.format(math.factorial(3)))
+print('The largest integer less than or equal to 5.6 is: {}'.format(math.floor(5.6)))
+print('The smallest integer greater than or equal to 5.6 is: {}'.format(math.ceil(5.6)))
+print('3 to the 5th power is: {}'.format(math.pow(3,5)))
+print('The math contant e to the power of 3 equals: {}'.format(math.exp(3)))
+print('The square root of 6 is {}'.format(math.sqrt(6)))
