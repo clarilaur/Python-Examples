1a38f2a56074ba82ca5dc2c9d5b76a19958e0f73
Laura <arnoldlaura24@gmail.com>
2025-01-16 12:45:26 -0800

    Create data_Type_Conversions.py

---
+++ Revision/data_Type_Conversions.py
@@ -0,0 +1,12 @@
+a = input()
+b = input()
+c = input()
+d = input()
+e = input()
+
+integer_sum_value = int(a) + int(b) + int(c) + int(d) + int(e)
+float_sum_value = float(a) + float(b) + float(c) + float(d) + float(e)
+string_sum_value= a + b + c + d + e
+print(f'Integer: {integer_sum_value}')
+print(f'Float: {float_sum_value}')
+print(f'String: {string_sum_value}')
