431007e1d40afa8aaf301dcb9345a1fb09c79e49
Laura <arnoldlaura24@gmail.com>
2025-01-23 18:23:05 -0800

    Create dictionary_Review.py

---
+++ dictionary_Review.py
@@ -0,0 +1,11 @@
+soundtrack_dic = {"The Bodyguard":"1992", "Saturday Night Fever":"1977"}
+print(soundtrack_dic.keys())
+
+print(soundtrack_dic.values())
+
+album_sales_dict = {"Back in Black": "50 Million", "The Bodyguard":"50 Million", "Thriller":"65 Million"}
+print(album_sales_dict)
+
+print(album_sales_dict["Thriller"])
+print(album_sales_dict.keys())
+print(album_sales_dict.values())
