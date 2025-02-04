ba8ab391fd6a621714e25574c8832773aa5ae2f0
Laura <arnoldlaura24@gmail.com>
2025-02-03 20:14:09 -0800

    Create replace_same_line.py

---
+++ Final Review/Functions, Lists, Exceptions/replace_same_line.py
@@ -0,0 +1,26 @@
+#Practice 1:
+
+sentence = input()
+
+switch = sentence.replace('good','great').lower()
+
+print(switch)
+
+#Practice 2:
+
+
+#Practice 3:
+
+sentence = input()
+
+switch = sentence.replace('hello','hi').strip()
+
+print(switch)
+
+#Practice 4:
+
+sentence = input()
+
+switch = sentence.replace('5','five').strip()
+
+print(switch)
