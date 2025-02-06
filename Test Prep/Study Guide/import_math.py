a20a2d1dac5ee08efa360295b6afe6649bc0b185
Laura <arnoldlaura24@gmail.com>
2025-02-05 18:33:30 -0800

    Create import_math.py

---
+++ Test Prep/Study Guide/import_math.py
@@ -0,0 +1,13 @@
+#import math module and call factorial()
+import math
+
+#solution accepts integer input
+num = int(input())
+#solution outputs the factorial of the integer input 
+result = math.factorial(num)
+#solution outputs Boolean identification of whether the factorial is >100
+
+print(result)
+
+determine = result > 100
+print(determine)
