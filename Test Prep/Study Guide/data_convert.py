90feceb2dbd6498386f5a8a14351bc5919eccab4
Laura <arnoldlaura24@gmail.com>
2025-02-05 19:47:23 -0800

    Create data_convert.py

---
+++ Test Prep/Study Guide/data_convert.py
@@ -0,0 +1,14 @@
+a = input()
+b = input()
+c = input()
+d = input()
+e = input()
+
+integer_sum_value = int(a)+int(b)+int(c)+int(d)+int(e)
+float_sum_value = float(a)+float(b)+float(c)+float(d)+float(e)
+string_sum_value = str(a)+str(b)+str(c)+str(d)+str(e)
+
+
+print(f'Integer: {integer_sum_value}')
+print(f'Float: {float_sum_value}')
+print(f'String: {string_sum_value}')
